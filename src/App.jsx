bae5cae6636845637989bf4eb344c5389029baa1
Salih Yıldız <salihyildiz060@gmail.com>
2024-12-10 09:35:20 +0300

    First props usage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./Footer.jsx";
 import Card from "./Card.jsx";
 import profilePic from "./assets/profile.jpg";
 import Button from "./Button/Button.jsx";
+import Student from "./Student.jsx";
 
 function App() {
   return (
@@ -10,6 +11,10 @@ function App() {
       <Header />
       <Card src={profilePic} title="Umutsuz Ev Adamları" description="Ankara" />
       <Button />
+      <Student name="Salih Yıldız" age={21} isStudent={true}/>
+      <Student name="Beyza NA" age={20} isStudent={true}/>
+      <Student name="Leyla" age={8} isStudent={false}/>
+      <Student />
       <Footer />
     </>
   );
--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,6 +1,11 @@
-import styles from './Button.module.css';
+import styles from "./Button.module.css";
 
 function Button() {
-    return(<button className={styles.button}>Click me</button>);
+  return (
+    <>
+      <button className={styles.successButton}>Click me</button>
+      <button className={styles.dangerButton}>Click me</button>
+    </>
+  );
 }
 export default Button;
