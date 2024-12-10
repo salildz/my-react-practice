0b3df12cf42cf37df74e1b0ae7461b124415fbb2
Salih Yıldız <salihyildiz060@gmail.com>
2024-12-10 09:21:32 +0300

    First module created

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Card from "./Card.jsx";
 import profilePic from "./assets/profile.jpg";
+import Button from "./Button/Button.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <Card src={profilePic} title="Umutsuz Ev Adamları" description="Ankara" />
+      <Button />
       <Footer />
     </>
   );
